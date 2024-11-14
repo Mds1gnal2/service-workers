7685492c4cebb2308a49f36805b87e33372c1557
Mds1gnal2 <167183857+Mds1gnal2@users.noreply.github.com>
2024-11-14 19:13:30 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,17 @@
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   const url = new URL(event.request.url);
-  if (url.protocol === 'custom-protocol:') {
+
+  // Interceptar solo solicitudes a Shodan.
+  if (url.hostname === 'www.shodan.io') {
     event.respondWith(
-      new Response('Esta es una respuesta personalizada del Service Worker', {
-        headers: { 'Content-Type': 'text/plain' }
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request).then(function(fetchResponse) {
+          // Almacena en caché la nueva respuesta.
+          return caches.open('shodan-cache').then(function(cache) {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
       })
     );
   }
